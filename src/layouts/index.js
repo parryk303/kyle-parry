f85cdd9d661f55b3b9d809d3a8d38e6b07681766
Kyle Parry <kyle.parry.303@gmail.com>
2021-05-28 19:50:37 -0600

    LinkedIn Refactor

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,21 +8,15 @@ import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
-    <meta property='og:title' content='Kyle Parry' />
-    <meta property='og:url' content='https://boring-franklin-f4b385.netlify.app/' />
-    <meta property='og:description' content='Kyle Parry Portfolio' />
-    <meta name='image' property='og:image' content='./preview.png'/>
-    <meta name='author' property='og:author' content='Kyle Parry' />
-
-    {/* <Helmet
+    <Helmet
       title='Kyle Parry'
       meta={[
-        { name: 'description', content: 'Check out my portfolio' },
-        { name: 'image',  property: 'og:image', content: './preview.png' },
+        { property: 'og:title', content: 'Kyle Parry'},
+        { property: 'og:description', content: 'Check out my portfolio' },
+        { property: 'og:image', content: './preview.png' },
         { property: 'og:url', content:'https://boring-franklin-f4b385.netlify.app/' }
       ]}
-    /> */}
-
+    />
     <Header />
     <About />
     <Extra />
